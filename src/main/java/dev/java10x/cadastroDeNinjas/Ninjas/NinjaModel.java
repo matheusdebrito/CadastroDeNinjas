874440cbb712e31214e0c5d1fa2debf744e434db
Matheus de Brito <matheusdebrito81@gmail.com>
2026-08-17 21:05:03 -0300

    fix: Coloca a coluna de email como unique

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaModel.java
@@ -18,33 +18,11 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private int idade;
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
